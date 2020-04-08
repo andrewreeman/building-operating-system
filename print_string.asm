7f355e733efa5cc2b90c9e33f755a507b9116e92
areeman <andrewreeman@gmail.com>
2020-04-08 15:50:18 +0100

    put_second_nibble requires less hardcoding

--- print_string.asm
+++ print_string.asm
@@ -1,12 +1,22 @@
 print_hex:
-    pusha
+    pusha    
+    call put_first_nibble    
+    call put_second_nibble
 
+    mov bx, HEX_OUT
+    call print_string   
+
+    popa
+    ret
+
+put_first_nibble:
+    pusha
     ; put first nibble
     mov bh, dh    
     shr bh, 4    
 
     cmp bh, 0x9
-    jg letter_mask
+    jg letter_mask        
 
     number_mask:
         or bh, 0x30    
@@ -18,13 +28,39 @@ print_hex:
 
     insert_char:
     mov al, bh
-    mov [HEX_OUT+2], al    
-
-    mov bx, HEX_OUT
-    call print_string   
-
+    mov [HEX_OUT+2], al   
     popa
-    ret
+    ret 
+
+put_second_nibble:
+     pusha
+    ; put second nibble
+    mov bh, dh    
+    and bh, 0x0f
+    mov cl, 3
+
+    cmp bh, 0x9
+    jg letter_mask_2 
+
+    number_mask_2:
+        or bh, 0x30    
+        jmp insert_char_2
+    
+    letter_mask_2:
+        sub bh, 9
+        or bh, 0x60            
+
+    insert_char_2:
+    mov al, bh
+    mov bx, HEX_OUT
+    add bl, cl
+    mov [bx], al
+    popa
+    ret 
+
+
+
+
 
 print_string:
     pusha ; store all register values on the stack
