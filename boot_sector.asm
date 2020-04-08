0f5049c2a38cebb52686b3d64c12cec7dc35dfdd
areeman <andrewreeman@gmail.com>
2020-04-08 04:36:16 +0100

    hello

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,5 +1,15 @@
-loop:
-    jmp loop
+mov ah, 0x0e ; tty
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $ ;inf loop
 
 times 510-($-$$) db 0
 
