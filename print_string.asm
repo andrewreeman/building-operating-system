02631e10a2690ad5878c10eaf5b29442c5b74978
areeman <andrewreeman@gmail.com>
2020-04-08 14:58:12 +0100

    Printing string implemented

--- print_string.asm
+++ print_string.asm
@@ -1,7 +1,13 @@
 print_string:
-    pusha
-    mov al, 'A'
-    mov ah, 0x0e
-    int 0x10
-    popa
-    ret
+    pusha ; store all register values on the stack
+
+    mov ah, 0x0e ; prepare to print tty
+    print_loop:
+        mov al, [bx] ; move character at bx into print register
+        int 0x10 
+
+        add bx, 0x01
+        cmp al, 0x0
+        jne print_loop
+    popa ; load all register values from the stack back into the registers
+    ret ; return to ip address that should now be on the stack
