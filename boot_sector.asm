4d2c2468620f8c05c39c8390c361f1ed6f70117d
areeman <andrewreeman@gmail.com>
2020-04-08 14:09:44 +0100

    Printing an X from data

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,15 +1,31 @@
+[org 0x7c00]
+
 mov ah, 0x0e ; tty
-mov al, 'H'
+
+; mov al, the_secret
+; int 0x10
+
+mov al, [the_secret]
 int 0x10
-mov al, 'e'
+
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
-mov al, 'l'
+
+mov bx, 0x29 ; 0x28 = 40
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
-int 0x10
-mov al, 'o'
+
+mov al, [0x7c01]
 int 0x10
 
 jmp $ ;inf loop
+
+the_secret:
+    db "X"
+    db "Y" 
 
 times 510-($-$$) db 0
 
