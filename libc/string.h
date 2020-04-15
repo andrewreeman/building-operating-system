12309a8953d0dc0bcdfbae8afe5642dd90b8cc29
areeman <andrewreeman@gmail.com>
2020-04-15 16:16:53 +0100

    with libc string instaed of util

---
+++ libc/string.h
@@ -0,0 +1,13 @@
+#ifndef STRING_H
+#define STRING_H
+
+void int_to_ascii(int n, char str[]);
+void reverse(char str[]);
+int strlen(char str[]);
+void backspace(char str[]);
+void append(char n, char str[]);
+int strcmp(char s1[], char s2[]);
+
+
+
+#endif
